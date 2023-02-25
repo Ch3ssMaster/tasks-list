21023e1eb4e782d51955c92b26bbfdb9a7ebc4a5
Ch3ssMaster <ajcmesa@gmail.com>
2023-02-25 22:19:00 +0100

    all tests ready

--- README.md
+++ README.md
@@ -13,8 +13,10 @@ git clone https://github.com/Ch3ssMaster/tasks-list
 ### Prerequisites
 
 You need to have the following installed on your machine:
+
 - Node.js
-- MongoDB
+- MongoDB and MongoDB Compass, then create a database called `tasks-list`
+- VS Code (optional)
 
 ### Installing
 
@@ -24,15 +26,45 @@ To install the dependencies, run the following command in the backend and fronte
 npm install
 ```
 
+#### Setting up the backend
+
+Create a .env file in the backend directory, and add the following:
+
+```bash
+MONGO_URI =  mongodb://localhost:27017/tasks-list
+TOKEN_SECRET =  5745cd8ca6d25082500c1de070652c8a8e30f0fa4296d18cb05c4985746e7588
+PORT =  8000
+```
+
+In case you want to mock tasks for the initial view, run the script:
+
+```bash
+npm run mock
+```
+
+This will generate a .mongodb file, in the path:
+
+`/backend/scripts/output/`
+
+That you can run with a click from the VS Code plugin playground.
+
 ## Running the tests (TODO)
 
-Explain how to run the automated tests for this system
+Unit tests have been carried out with the JEST library, both on the front end and on the back end.
 
-### Sample Tests
+### Backend tests
 
-Explain what these tests test and why
+The libraries used for backend testing are:
 
-    Give an example
+- [JEST](https://jestjs.io/) - The testing framework used
+- [Supertest](https://www.npmjs.com/package/supertest) - The library used to test the API
+- [Cross-env](https://www.npmjs.com/package/cross-env) - The library used to set the environment variables
+
+To run the tests, execute the following command:
+
+```bash
+npm run test
+```
 
 ### Style test
 
@@ -46,11 +78,10 @@ Add additional notes to deploy this on a live system
 
 ## Built With
 
- - [React](https://reactjs.org/) - The frontend framework used
- - [Node.js](https://nodejs.org/en/) - The backend framework used
- - [Express](https://expressjs.com/) - The backend framework used
- - [MongoDB](https://www.mongodb.com/) - The database used
-
+- [React](https://reactjs.org/) - The frontend framework used
+- [Node.js](https://nodejs.org/en/) - The backend framework used
+- [Express](https://expressjs.com/) - The backend framework used
+- [MongoDB](https://www.mongodb.com/) - The database used
 
 ## Contributing
 
@@ -59,12 +90,12 @@ of conduct, and the process for submitting pull requests to us.
 
 ## Versioning
 
-We use [Semantic Versioning](http://semver.org/) for versioning. 
+We use [Semantic Versioning](http://semver.org/) for versioning.
 
 ## Authors
 
-  - **Ch3ssMaster** - *Tasks List* -
-    [Ch3ssMaster](https://github.com/Ch3ssMaster)
+- **Ch3ssMaster** - _Tasks List_ -
+  [Ch3ssMaster](https://github.com/Ch3ssMaster)
 
 ## License
 
--- backend/tests/task.test.js
+++ backend/tests/task.test.js
@@ -1,5 +1,7 @@
 // importar mongoose y supertest
 const mongoose = require("mongoose");
+// importar jsonwebtoken 
+const jwt = require("jsonwebtoken");
 // importar la librería para testear las solicitudes http
 const request = require("supertest");
 // importar el servidor
@@ -10,8 +12,10 @@ require("dotenv").config();
 // incluir el token para las pruebas
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NzczNDI3NTQsImV4cCI6MTY3NzYwMTk1NH0.x1S0V3UpKKqHc84VLw4Xj7DNN7dh5VdeR7kmL5rR6D8";
-
-/* Connecting to the database before each test. */
+const EXPIRED_TOKEN = jwt.sign({}, process.env.TOKEN_SECRET, {
+  expiresIn: "0",
+});
+// conectar a la base de datos antes de ejecutar las pruebas
 beforeEach(async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -21,7 +25,7 @@ beforeEach(async () => {
   }
 });
 
-/* Closing database connection after each test. */
+// cerrar la conexión a la base de datos después de ejecutar las pruebas
 afterEach(async () => {
   try {
     await mongoose.connection.close();
@@ -31,10 +35,153 @@ afterEach(async () => {
   }
 });
 
+// comprobar que la ruta GET / devuelve todas las tareas
 describe("GET /", () => {
+  // en caso de incluir el token en el header, la respuesta debe ser 200
   it("should return all tasks", async () => {
     const res = await request(app).get("/").set("auth-token", `${TOKEN}`);
     expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe("succeeded");
+    expect(res.body.data).toBeDefined();
     expect(res.body.data.length).toBeGreaterThan(0);
+    expect(res.body.error).toBe(null);
+  });
+
+  // en caso de no incluir el token en el header, la respuesta debe ser 401
+  it("should return 401", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Access Denied");
+  });
+
+  // en caso de incluir un token caducado en el header, la respuesta debe ser 400
+  it("should return 400", async () => {
+    const res = await request(app)
+      .get("/")
+      .set("auth-token", `${EXPIRED_TOKEN}`);
+    expect(res.statusCode).toBe(400);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Expired Token");
+  });
+});
+
+// comprobar que la ruta POST / crea una nueva tarea
+describe("POST /", () => {
+  // con token
+  it("should create a new task", async () => {
+    const res = await request(app)
+      .post("/")
+      .set("auth-token", `${TOKEN}`)
+      .send({ title: "Test task" });
+    expect(res.statusCode).toBe(201);
+    expect(res.body.status).toBe("succeeded");
+    expect(res.body.data._id).toBeDefined();
+    expect(res.body.data.title).toBe("Test task");
+    expect(res.body.data.done).toBe(false);
+    expect(res.body.error).toBe(null);
+  });
+
+  // sin token
+  it("should return 401", async () => {
+    const res = await request(app).post("/").send({ title: "Test task" });
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Access Denied");
+  });
+});
+
+// comprobar que la ruta PATCH /:id actualiza una tarea
+describe("PATCH /:id", () => {
+  // actualizar el campo title de una tarea
+  it("should update a task title", async () => {
+    // obtener el id de la última tarea creada
+    const data = await request(app).get("/").set("auth-token", `${TOKEN}`);
+    const lastTaskId = data.body.data[data.body.data.length - 1]._id;
+    const res = await request(app)
+      .patch(`/${lastTaskId}`)
+      .set("auth-token", `${TOKEN}`)
+      .send({ title: "Updated task" });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe("succeeded");
+    expect(res.body.data.title).toBe("Updated task");
+    expect(res.body.error).toBe(null);
+  });
+  // actualizar el campo done de una tarea
+  it("should update a task done", async () => {
+    // obtener el id de la última tarea creada
+    const data = await request(app).get("/").set("auth-token", `${TOKEN}`);
+    const lastTaskId = data.body.data[data.body.data.length - 1]._id;
+    const res = await request(app)
+      .patch(`/${lastTaskId}`)
+      .set("auth-token", `${TOKEN}`)
+      .send({ done: true });
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe("succeeded");
+    expect(res.body.data.done).toBe(true);
+    expect(res.body.error).toBe(null);
+  });
+
+  // comprobar que la ruta PATCH /:id devuelve un error si el id no existe
+  it("should return an error if the id does not exist", async () => {
+    const res = await request(app)
+      .patch("/5f9f1b9b9b9b9b9b9b9b9b9b")
+      .set("auth-token", `${TOKEN}`)
+      .send({ done: true });
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Task not found");
+  });
+
+  // sin token
+  it("should return 401", async () => {
+    const res = await request(app)
+      .patch("/5f9f1b9b9b9b9b9b9b9b9b9b")
+      .send({ done: true });
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Access Denied");
+  });
+});
+
+// comprobar que la ruta DELETE /:id elimina una tarea
+describe("DELETE /:id", () => {
+  // eliminar la última tarea
+  it("should delete the last task", async () => {
+    // obtener el id de la última tarea
+    const data = await request(app).get("/").set("auth-token", `${TOKEN}`);
+    const lastTaskId = data.body.data[data.body.data.length - 1]._id;
+    const res = await request(app)
+      .delete(`/${lastTaskId}`)
+      .set("auth-token", `${TOKEN}`);
+    expect(res.statusCode).toBe(200);
+    expect(res.body.status).toBe("succeeded");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe(null);
+  });
+
+  // comprobar que la ruta DELETE /:id devuelve un error si el id no existe
+  it("should return an error if the id does not exist", async () => {
+    const res = await request(app)
+      .delete("/5f9f1b9b9b9b9b9b9b9b9b9b")
+      .set("auth-token", `${TOKEN}`);
+    expect(res.statusCode).toBe(404);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Task not found");
+  });
+
+  // sin token
+  it("should return 401", async () => {
+    const res = await request(app).delete("/5f9f1b9b9b9b9b9b9b9b9b9b");
+    expect(res.statusCode).toBe(401);
+    expect(res.body.status).toBe("failed");
+    expect(res.body.data).toBe(null);
+    expect(res.body.error).toBe("Access Denied");
   });
 });
